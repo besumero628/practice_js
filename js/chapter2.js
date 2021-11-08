da89252f8cfdd132af31699d23db6f5a3646bd79
besumero628 <besumero628@gmail.com>
2021-11-08 22:50:48 +0900

    janken

--- js/chapter2.js
+++ js/chapter2.js
@@ -13,16 +13,30 @@
 
 // alert('あなたのBMIは' + bmi + "です。")
 
-// Humberger
-let budget = prompt('所持金はいくらですか？');
-budget = parseFloat(budget);
+// // Humberger
+// let budget = prompt('所持金はいくらですか？');
+// budget = parseFloat(budget);
 
-let flag = confirm('お腹は空いていますか？');
+// let flag = confirm('お腹は空いていますか？');
 
-if(budget >= 1500 && flag){
-  alert('ピザを注文しました。')
-} else if(budget >= 500 && flag){
-  alert('ポテトを注文しました。')
-} else if(flag) {
-  alert('節約しましょう。')
+// if(budget >= 1500 && flag){
+//   alert('ピザを注文しました。')
+// } else if(budget >= 500 && flag){
+//   alert('ポテトを注文しました。')
+// } else if(flag) {
+//   alert('節約しましょう。')
+// }
+
+// janken
+const GU    = 1;
+const CHOKI = 2;
+const PA    = 3;
+
+let hum = prompt('1〜3の数値を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー\n')
+hum = parseInt(hum, 10)
+
+if (hum != GU && hum != CHOKI && hum != PA){
+  alert('入力値を上手く認識できませんでした。\nブラウザを再読み込みすると、もう一度挑戦できます。')
+} else {
+  
 }
