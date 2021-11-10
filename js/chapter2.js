5ab0ce10654020288af74267be8797fc702a3c0a
besumero628 <besumero628@gmail.com>
2021-11-10 14:10:23 +0900

    janken kai

--- js/chapter2.js
+++ js/chapter2.js
@@ -32,11 +32,39 @@ const GU    = 1;
 const CHOKI = 2;
 const PA    = 3;
 
-let hum = prompt('1〜3の数値を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー\n')
-hum = parseInt(hum, 10)
+let hum = prompt('1〜3の数値を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー\n');
+hum = parseInt(hum, 10);
 
 if (hum != GU && hum != CHOKI && hum != PA){
-  alert('入力値を上手く認識できませんでした。\nブラウザを再読み込みすると、もう一度挑戦できます。')
+  alert('入力値を上手く認識できませんでした。\nブラウザを再読み込みすると、もう一度挑戦できます。');
 } else {
+  let com = Math.floor(Math.random() * 3) + 1;
+
+  let comHandName = '';
+  switch (com) {
+    case GU:
+      comHandName = 'グー'
+      break;
+
+    case CHOKI:
+      comHandName = 'チョキ'
+      break;
+
+    case PA:
+      comHandName = 'パー'
+      break;
+  }
+
+  let msgResult = '';
+  if(hum === com) {
+    msgResult = '結果はあいこでした。'
+  } else if((com === GU && hum === PA) || (com === CHOKI && hum === GU) || (com === PA && hum === CHOKI) ) {
+    msgResult = '勝ちました。'
+  } else {
+    msgResult = '負けました。'
+  }
   
+  alert(msgResult + '対戦相手は「' + comHandName + '」を出しました。')
+
 }
+
