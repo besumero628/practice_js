c3dc4747536b1500c20dafcf483f373a0fff0436
besumero628 <besumero628@gmail.com>
2021-11-12 23:45:17 +0900

    function2

--- js/chapter4.js
+++ js/chapter4.js
@@ -1,8 +1,16 @@
+// test
 let n = prompt('名前を入力してください');
 greet(n);
+greet2(n);
 
 function greet(name) {
   let message = 'こんにちは'
   alert(message + name);
+  return;
+}
+
+function greet2(name) {
+  let message = 'こんにちは'
+  alert(name);
   return;
 }
