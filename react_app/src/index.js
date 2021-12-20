d2fd4abf4cdc63f73e8df76c7bb11f541b2224be
besumero628 <besumero628@gmail.com>
2021-12-20 17:51:12 +0900

    [React] コンポーネントに属性を付与する

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title = "App"
+    message = "This is App Component!" />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
