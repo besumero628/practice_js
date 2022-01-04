0195d2a796a7aeb8372e0c792533522bcfd509ec
besumero628 <besumero628@gmail.com>
2022-01-04 22:56:20 +0900

    [React] routerの設定

--- react-router-mui/src/App.js
+++ react-router-mui/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Contact() {
+  return <h2>Contact</h2>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Hello React Router</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}> /</Route>
+        <Route path="/contact" element={<Contact />}> /</Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
